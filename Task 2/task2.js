5575006775788dbb37ce53daac83bf6c417f85a1
zahranniyas <zahranniyas@gmail.com>
2022-10-09 10:24:29 +0530

    task 2 of mid sem quiz

---
+++ Task 2/task2.js
@@ -0,0 +1,23 @@
+var movies = ["Inception","Shutter Island","Shawshank Redemption","The Dark Knight","Infinity War"];
+var fourthMovieVal = movies[3];
+if (fourthMovieVal.charAt(0)==="I"){
+    console.log("Yay");
+}
+else {
+    console.log("Nay");
+}
+
+movies = [];
+movies.push("Inception");
+movies.push("Shutter Island");
+movies.push("Shawshank Redemption");
+movies.push("Iron Man");
+movies.push("Infinity War");
+
+var fourthMovieVal = movies[3];
+if (fourthMovieVal.charAt(0)==="I"){
+    console.log("Yay");
+}
+else {
+    console.log("Nay");
+}
